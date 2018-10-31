cabc2e124da88d2ba142db26541bfed221609d4a
Pipe <mfavendano@poligran.edu.co>
2018-10-31 18:34:21 -0500

    Juego Calculadora

--- app/src/main/java/co/edu/poli/gamification/poliplay/JuegoCalculadora.java
+++ app/src/main/java/co/edu/poli/gamification/poliplay/JuegoCalculadora.java
@@ -16,9 +16,33 @@ public class JuegoCalculadora extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego_calculadora);
+        dato1 = (EditText) findViewById(R.id.dato1);
+        dato2 = (EditText) findViewById(R.id.dato2);
+        dato3 = (EditText) findViewById(R.id.dato3);
+        dato4 = (EditText) findViewById(R.id.dato4);
+        dato5 = (EditText) findViewById(R.id.dato5);
+        dato6 = (EditText) findViewById(R.id.dato6);
+        dato7 = (EditText) findViewById(R.id.dato7);
+
+        resultado = (TextView) findViewById(R.id.resultado);
+
+
     }
     public  void suma (View view){
+        int dat1, dat2, dat3, dat4, dat5, dat6, dat7, res;
 
+        dat1 = Integer.parseInt(dato1.getText().toString());
+        dat2 = Integer.parseInt(dato2.getText().toString());
+        dat3 = Integer.parseInt(dato3.getText().toString());
+        dat4 = Integer.parseInt(dato4.getText().toString());
+        dat5 = Integer.parseInt(dato5.getText().toString());
+        dat6 = Integer.parseInt(dato6.getText().toString());
+        dat7 = Integer.parseInt(dato7.getText().toString());
+
+
+        res = dat1+dat2+dat3+dat7+dat6+dat5+dat4;
+
+        resultado.setText(String.valueOf(res));
     }
 
 }
